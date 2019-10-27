8bdbee863903def7c0da70f62f41209315cca0bb
CleoGitHub <survcleo@hotmail.fr>
2019-10-27 16:27:55 +0100

    Push only master branch

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Entity\Container;
 use App\Entity\SuperClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +21,7 @@ class DefaultController extends AbstractController
         $articles = $this->getDoctrine()->getRepository(Container::class)->findAll()[0]->getChilds();
         return $this->render('default/index.html.twig', [
             'articles' => $articles,
+            'c' => $this->getDoctrine()->getRepository(Client::class)->findOneByNom('Brassens')
         ]);
     }
 }
